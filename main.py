9c52b5a0b47a58d68c72d1bb414e8df2baed8c4d
Felipe Spitale <103086933+Lipe333@users.noreply.github.com>
2026-03-12 06:29:56 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,13 +2,23 @@
 #  Taken from https://medium.com/data-science/building-mobilenet-from-scratch-using-tensorflow-ad009c5dd42c
 
 import tensorflow as tf
-
+import numpy as np  
+from sklearn.utils.class_weight import compute_class_weight
 from tensorflow.keras.layers import Input, DepthwiseConv2D, GlobalAveragePooling2D
-from tensorflow.keras.layers import Conv2D, BatchNormalization
+from tensorflow.keras.layers import Conv2D, BatchNormalization, Rescaling, Dropout
 from tensorflow.keras.layers import ReLU, AvgPool2D, Flatten, Dense
 from tensorflow.keras import Model
 
 NUM_CLASSES = 10
+
+#evita alocar toda a memória da GPU
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        print(e)
 
 def mobilenetBlock(x, filters, strides):
     x = DepthwiseConv2D(kernel_size=3, strides=strides, padding='same')(x)
@@ -21,8 +31,9 @@ def mobilenetBlock(x, filters, strides):
 
     return x
 
-input = Input(shape = (128,128,3))
-x = Conv2D(filters = 32, kernel_size = 3, strides = 2, padding = 'same')(input)
+inputs = Input(shape = (128,128,3))
+x = Rescaling(1./255)(inputs)
+x = Conv2D(filters = 32, kernel_size = 3, strides = 2, padding = 'same')(x)
 x = BatchNormalization()(x)
 x = ReLU()(x)
 
@@ -39,19 +50,15 @@ x = mobilenetBlock(x, filters = 1024, strides = 2)
 x = mobilenetBlock(x, filters = 1024, strides = 1)
 
 x = GlobalAveragePooling2D()(x)
-x = Flatten()(x)
+x = Dropout(0.3)(x) # Adicionando dropout para reduzir overfitting
 output = Dense (units = NUM_CLASSES, activation = 'softmax')(x) #10 classes 
-model = Model(inputs=input, outputs=output)
+model = Model(inputs=inputs, outputs=output)
 model.summary()
 
 train_ds = tf.keras.utils.image_dataset_from_directory(
     "croppedImages_aug/train",
-    validation_split=0.2,
-    subset="training",
-    seed=123,
     image_size=(128, 128),
     batch_size=32)
-
 
 val_ds = tf.keras.utils.image_dataset_from_directory(
     "croppedImages_aug/val",
@@ -59,10 +66,6 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
     batch_size=32,
     shuffle=False
 )
-
-# train_ds = train_ds.map(augment, num_parallel_calls=tf.data.AUTOTUNE)
-# train_ds = train_ds.prefetch(buffer_size=tf.data.AUTOTUNE)
-# val_ds = val_ds.prefetch(buffer_size=tf.data.AUTOTUNE)
 
 model.compile(
     optimizer=tf.keras.optimizers.Adam(1e-3),
@@ -85,21 +88,33 @@ early_stopping = tf.keras.callbacks.EarlyStopping(
     restore_best_weights=True
 )
 
-#evita alocar toda a memória da GPU
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
-    try:
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-    except RuntimeError as e:
-        print(e)
+reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(
+    monitor='val_loss',
+    factor=0.5,
+    patience=3,
+    min_lr=1e-6,
+    verbose=1
+)
+
+labels = np.array([y.numpy() for x, y in train_ds.unbatch()])
+
+weights = compute_class_weight(
+    class_weight="balanced",
+    classes=np.unique(labels),
+    y=labels
+)
+
+class_weights = dict(enumerate(weights))
+
+print(class_weights)
 
 history = model.fit(
     train_ds,
     validation_data=val_ds,
     epochs=20,
-    callbacks=[checkpoint, early_stopping]
+    callbacks=[checkpoint, early_stopping, reduce_lr],
+    class_weight=class_weights
 )
 
 
-# tf.keras.utils.plot_model(model, to_file='model.png', show_shapes=True, show_dtype=False,show_layer_names=True, rankdir='TB', expand_nested=False, dpi=96)
+# train loss desce mas não tem melhora na val accuracy, provavelmente overfitting.
